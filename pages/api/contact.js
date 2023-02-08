de4f8397691b6fca2cd9822a25d956f6f5108347
Gopal-Bhattarai <gbhattarai55@gmail.com>
2023-02-08 12:40:00 +0545

    contact API fullName to just name changed

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -5,10 +5,11 @@ sgMail.setApiKey(process.env.SGMAIL_API)
 export default async function handler(req, res){
 
     const newEmail = req.body;
+    console.log(newEmail)
     await sgMail.send({
         to: 'gbhattarai55@gmail.com',
         from: 'gbhattarai55@gmail.com',
-        subject: newEmail.fullName,
+        subject: newEmail.name,
         text: newEmail.email + '\n\n' + newEmail.message
     })
     res.status(200).json({'status':'Email sent successfully!'})
